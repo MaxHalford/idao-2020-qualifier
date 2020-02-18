ca025d5448978456613588639d8743172b8404e2
raphaelsty <raphael.sourty@gmail.com>
2020-02-18 19:01:57 +0100

    Updating the pv function to equin. We try to change the data space during the competition but this does not improve the quality of our model. The estimation of the velocity is less good in the equinox space.

--- switch_space.py
+++ switch_space.py
@@ -89,123 +89,104 @@ def equin_to_pv(a, ex, ey, hx, hy, hz, mu = 398600441500000):
     """
     # CL_kp_anomConvertCir Func
     # CL_kp_anomConvertCir(type_anom1,type_anom2,ex,ey,pso1)
-    # F = CL_kp_anomConvertCir("M", "E", ex, ey, equin(6,:));
+    # F = CL_kp_anomConvertCir("M", "E", ex, ey, equin(6,:))
     pso1 = hz
     ecc = np.sqrt(ex**2 + ey**2)
     A = math.atan2(ey, ex)
 
     anom1 =  pso1 - A
 
+
     # Ellipse
-    if ecc < 1:
-        # CL__kp_M2Eell Func
-        # CL_kp_M2E(e,M)
-        # CL__kp_M2Eell(ecc(I), M(I))
+    ### if ecc < 0.98:
+    # CL__kp_M2Eell Func
+    # CL_kp_M2E(e,M)
+    # CL__kp_M2Eell(ecc(I), M(I))
+    M = anom1
+    e = ecc
 
-        M = anom1
-        e = ecc
+    # CL_rMod Func
+    # CL_rMod(x, a, b)
+    # CL_rMod(M,-%pi,%pi)
+    delta = math.pi - (- math.pi)
+    nrev = np.floor((M - (- math.pi)) / delta)
+    reduced_m = M - nrev * delta
 
-        # CL_rMod Func
-        # CL_rMod(x, a, b)
-        # CL_rMod(M,-%pi,%pi)
-        delta = math.pi - (- math.pi)
-        nrev = np.floor((M - (- math.pi)) / delta)
-        reduced_m = M - nrev * delta
+    # crbt Func
+    crbt = lambda x: np.sign(x) * np.abs(x)**(1/3)
 
-        k1 = 3 * math.pi + 2
-        k2 = math.pi - 1
-        k3 = 6 * math.pi - 1
-        A  = 3 * k2 * k2 / k1
-        B  = k3 * k3 / (6 * k1)
+    k1 = 3 * math.pi + 2
+    k2 = math.pi - 1
+    k3 = 6 * math.pi - 1
+    A  = 3 * k2 * k2 / k1
+    B  = k3 * k3 / (6 * k1)
 
-        if abs(reduced_m) < (1./ 6.):
-            E = reduced_m + e * (cbrt(6 * reduced_m) - reduced_m)
+    E = 0
 
-        if abs(reduced_m) >= (1./ 6.) and reduced_m < 0:
-            w = math.pi + reduced_m
-            E = reduced_m + e * (math.pi - A * w / (B - w) - reduced_m)
+    if abs(reduced_m) < (1./ 6.):
+        E = reduced_m + e * (crbt(6 * reduced_m) - reduced_m)
 
-        if abs(reduced_m) >= (1./ 6.) and  reduced_m >= 0:
-            w = math.pi - reduced_m
-            E = reduced_m + e * (math.pi - A * w / (B - w) - reduced_m)
+    if abs(reduced_m) >= (1./ 6.) and reduced_m < 0:
+        w = math.pi + reduced_m
+        E = reduced_m + e * (math.pi - A * w / (B - w) - reduced_m)
 
-        e1 = 1 - e
+    if abs(reduced_m) >= (1./ 6.) and  reduced_m >= 0:
+        w = math.pi - reduced_m
+        E = reduced_m + e * (math.pi - A * w / (B - w) - reduced_m)
 
-        no_cancellation_risk = (e1 + E * E / 6) >= 0.1
+    e1 = 1 - e
 
-        f = 0
-        fd = 0
+    no_cancellation_risk = (e1 + E * E / 6) >= 0.1
 
-        for i in range(2):
+    f = 0
+    fd = 0
 
-            fdd = e * np.sin(E)
-            fddd = e * np.cos(E)
+    for i in range(100):
 
-            if no_cancellation_risk:
+        fdd = e * np.sin(E)
+        fddd = e * np.cos(E)
 
-                f = (E - fdd) - reduced_m
-                fd = 1 - fddd
+        if no_cancellation_risk:
 
-            if not no_cancellation_risk:
-                # CL__E_esinE Func
-                # f(I)  = CL__E_esinE(e(I),E(I)) - reducedM(I);
-                # CL__E_esinE(e,E)
-                x = (1 - e) * np.sin(E)
-                mE2 = -E * E
-                term = E
-                d = 0
-                x0 = None
-                iteration = 0
-                nb_max_iter = 20
-                K = True
-                while K and iteration <= nb_max_iter:
-                    d = d + 2
-                    term = term * mE2 / (d * (d + 1))
-                    x0 = x
-                    x = x - term
-                    # If x == x0: Stop
-                    if x == x0:
-                        K = False
-                    iteration += 1
-                # Output CL__E_esinE
-                f = x
+            f = (E - fdd) - reduced_m
+            fd = 1 - fddd
 
-                s = np.sin(0.5 * E)
-                fd = e1 + 2 * e * s * s
+        if not no_cancellation_risk:
+            # CL__E_esinE Func
+            # f(I)  = CL__E_esinE(e(I),E(I)) - reducedM(I)
+            # CL__E_esinE(e,E)
+            x = (1 - e) * np.sin(E)
+            mE2 = -E * E
+            term = E
+            d = 0
+            x0 = None
+            iteration = 0
+            nb_max_iter = 20
+            K = True
+            while K and iteration <= nb_max_iter:
+                d = d + 2
+                term = term * mE2 / (d * (d + 1))
+                x0 = x
+                x = x - term
+                # If x == x0: Stop
+                if x == x0:
+                    K = False
+                iteration += 1
+            # Output CL__E_esinE
+            f = x
 
-            dee = f * fd / (0.5 * f * fdd - fd * fd)
-            w = fd + 0.5 * dee * (fdd + dee * fddd / 3)
-            fd = fd + dee * (fdd + 0.5 * dee * fddd)
-            E = E - (f - dee * (fd - w)) / fd
+            s = np.sin(0.5 * E)
+            fd = e1 + 2 * e * s * s
 
-        E = E + (M - reduced_m)
-    """
-    # Hyperbola
-    if ecc > 1:
-        # CL__kp_M2Ehyp Func
-        # E(I) = CL__kp_M2Ehyp(ecc(I), M(I));
-        # CL__kp_M2Ehyp(ecc,M)
+        dee = f * fd / (0.5 * f * fdd - fd * fd)
+        w = fd + 0.5 * dee * (fdd + dee * fddd / 3)
+        fd = fd + dee * (fdd + 0.5 * dee * fddd)
+        E = E - (f - dee * (fd - w)) / fd
 
-        E = math.asinh(M / ecc)
+    E = E + (M - reduced_m)
 
-        if (ecc - 1)**2 + (0.5 * np.abs(M) - 1)**2 < 1.01:
-            E = np.sign(M) * (6 * np.abs(M) / ecc) ** (1/3)
-
-        nb_max_iter = 20
-        errmax = 0.0000000000000222044605
-        iteration = 0
-        K = True
-        while K and iteration <= nb_max_iter:
-            E1 = E
-            E = E - (ecc * np.sinh(E) - E - M) / (ecc * cosh(E) - 1)
-            err1 = abs(E1 - E) / max(abs(E),1)
-            err2 = abs(ecc * np.sinh(E) - E - M) / max(abs(M),1)
-            if err1 <= errmax and err2 <= errmax:
-                K = False
-            iteration += 1
-
-    """
     anom2 = E
+
     pso_2 = anom2 + math.atan2(ey, ex)
 
     F = pso_2
@@ -240,4 +221,4 @@ def equin_to_pv(a, ex, ey, hx, hy, hz, mu = 398600441500000):
     pos = P * X + Q * Y
     vel = P * VX + Q * VY
 
-    return pos, vel
+    return pos[0], pos[1], pos[2], vel[0], vel[1], vel[2]
